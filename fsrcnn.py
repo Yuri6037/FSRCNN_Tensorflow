a038b985f08f2305d09a8a28ea93bba3badd24b2
Yuri Edward <yuri6037@outlook.com>
2021-10-13 15:48:50 +0200

    Attempt at fixing coding errors due to stupid python3

--- fsrcnn.py
+++ fsrcnn.py
@@ -5,11 +5,12 @@ import tensorflow as tf
 import numpy as np 
 import os
 
-def model(x, y, lr_size, scale, batch, lr, d, s, m):
+def model(x, y, lr_size, scale, batch, lr, stupid_python3):
     """
     Implementation of FSRCNN: http://mmlab.ie.cuhk.edu.hk/projects/FSRCNN.html.
     """
-    
+
+    d, s, m = stupid_python3
     channels = 1
     PS = channels * (scale*scale) # for sub-pixel, PS = Phase Shift
     bias_initializer = tf.constant_initializer(value=0.0)
